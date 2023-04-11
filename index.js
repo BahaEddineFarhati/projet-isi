083beab6fb045786763c1f46e8a5921a1b9599eb
BahaEddineFarhati <bahaeddine.farhati1@gmail.com>
2023-04-11 16:42:21 +0100

    dashboard now working :)

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-//test
+
+
+window.onload = () => {
+    var myButtons = document.querySelectorAll(".sec_title");
+    myButtons.forEach(function(myButton) {
+    
+        const page_id = myButton.dataset.tab;
+      myButton.addEventListener("click", function() {
+        document.querySelector('.dash_btn .page.is-active').classList.remove('is-active');
+        myButton.parentNode.classList.add('is-active');
+        SwitchPage(page_id)
+      });
+    });
+}
+
+function SwitchPage (page_id) {
+    console.log(page_id);
+
+    const current_page = document.querySelector('.pages .page.is-active');
+    current_page.classList.remove('is-active');
+
+    const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+    next_page.classList.add('is-active');
+}
